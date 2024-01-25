ef8b07e7b7b97a1aa2f9e64c4c9efbc8406b82f7
kmkcc2 <kmk.krowicki@gmail.com>
2024-01-25 18:30:38 +0100

    added routes and authorization middleware

---
+++ src/middleware/authorize.ts
@@ -0,0 +1,69 @@
+import { type Response, type Request, type NextFunction } from 'express'
+import jwt, { JwtPayload, TokenExpiredError } from 'jsonwebtoken'
+import { decode } from 'jsonwebtoken'
+
+export function authorizeUser (req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization
+  try {
+    if (!authHeader) {
+      throw new Error('Authorization header is missing')
+    }
+    const token = authHeader.split(' ')[1]
+    const SECRET = process.env.ACCESS_TOKEN_SECRET as string
+    if (token === null) return res.status(401).send({ message: 'Unauthorized' })
+    jwt.verify(token, SECRET, (err, user) => {
+      if (err != null) {
+        if (err instanceof TokenExpiredError) {
+          return res.status(403).send({ message: 'Token has expired, please sign in again' })
+        }
+        return res.status(403).send({ message: 'Invalid token signature' })
+      }
+      const decoded: JwtPayload = decode(token) as JwtPayload
+      if (
+        decoded !== null &&
+        Object.prototype.hasOwnProperty.call(decoded, 'role') &&
+        decoded.role !== 'user'
+      ) {
+        return res.status(401).send({ message: 'Unauthorized' })
+      }
+      next()
+    })
+  } catch (err) {
+    const msg = err instanceof Error ? 'Unauthorized. ' + err.message : 'Unauthorized'
+    return res.status(401).send({ message: msg })
+  }
+}
+export function authorizeAdmin(req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization
+  try {
+    if (!authHeader) {
+      throw new Error('Authorization header is missing')
+    }
+    const token = authHeader.split(' ')[1]
+    const SECRET = process.env.ACCESS_TOKEN_SECRET as string
+    if (token === null) return res.status(401).send({ message: 'Unauthorized' })
+    jwt.verify(token, SECRET, (err, user) => {
+      if (err !== null) {
+        if (err instanceof TokenExpiredError) {
+          return res
+            .status(403)
+            .send({ message: 'Token has expired, please sign in again' })
+        }
+        return res.status(403).send({ message: 'Invalid token signature' })
+      }
+      const decoded: JwtPayload = decode(token) as JwtPayload
+      if (
+        decoded !== null &&
+        Object.prototype.hasOwnProperty.call(decoded, 'role') &&
+        decoded.role !== 'admin'
+      ) {
+        return res.status(401).send({ message: 'Unauthorized' })
+      }
+      next()
+    })
+  } catch (err) {
+    const msg =
+      err instanceof Error ? 'Unauthorized. ' + err.message : 'Unauthorized'
+    return res.status(401).send({ message: msg })
+  }
+}
